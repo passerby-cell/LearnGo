359de253a78dac30e801f9941ad9bcfd05739b3e
LawInBee <18606145168@163.com>
2023-02-08 02:15:33 +0800

    init Learn Go 数组

---
+++ 数组/ReaDME.md
@@ -0,0 +1,7 @@
+# 数组
+```go
+var arr1 [5]int
+arr2:=[3]int{1,2,3}
+arr3:=[...]int{1,2,3}
+var grid [4][5]bool
+```
--- 数组/basic.go
+++ 数组/basic.go
@@ -1,5 +1,8 @@
 package main
 
 func main() {
-
+	var arr1 [5]int
+	arr2 := [3]int{1, 2, 3}
+	arr3 := [...]int{1, 2, 3}
+	var grid [4][5]bool
 }
