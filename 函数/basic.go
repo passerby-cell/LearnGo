515f539d33050ae55f88864dc33204b401fee373
LawInBee <18606145168@163.com>
2023-02-08 01:41:21 +0800

    init Learn Go 函数

--- 函数/basic.go
+++ 函数/basic.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+func eval(a, b int, op string) int {
+	switch op {
+	case "+":
+		return a + b
+	case "-":
+		return a - b
+	case "*":
+		return a * b
+	case "/":
+		return a / b
+	default:
+		panic("不支持的运算符" + op)
+	}
+}
+
+func div(a, b int) (q, r int) {
+	return a / b, a % b
+}
+func main() {
+	fmt.Println(eval(2, 4, "+"))
+	fmt.Println(div(21, 4))
+	q, r := div(21, 4)
+	fmt.Println(q, r)
 }
